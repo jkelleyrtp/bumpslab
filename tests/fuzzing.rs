de8f40d110322367119a006e84c04e14ffdc9bc6
Evan Almloff <ealmlof1@stumail.jccc.edu>
2023-01-23 10:13:51 -0600

    make fuzzing test more complete

--- tests/fuzzing.rs
+++ tests/fuzzing.rs
@@ -8,11 +8,20 @@ fn fuzzing() {
     for _ in 0..1000 {
         if rand::random() {
             for _ in 0..rand::random::<usize>() % 10 {
-                values.push(slab.push(0));
+                let slot = slab.push(1234);
+                unsafe {
+                    assert_eq!(&*slot.ptr(), &1234);
+                }
+                values.push(slot);
             }
         } else {
             for _ in 0..rand::random::<usize>() % 10 {
-                if let Some(slot) = values.pop() {
+                if !values.is_empty() {
+                    let idx = rand::random::<usize>() % values.len();
+                    unsafe {
+                        assert_eq!(&*values[idx].ptr(), &1234);
+                    }
+                    let slot = values.remove(idx);
                     slab.remove(slot);
                 }
             }
